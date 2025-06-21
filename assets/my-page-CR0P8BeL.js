abfe6ab72f0e3b8c2fa33c66217080f619a46af5
arthurcadore <arthurbarcella.ifsc@gmail.com>
2025-06-21 01:19:44 -0300

    Updates

---
+++ assets/my-page-CR0P8BeL.js
@@ -0,0 +1,1 @@
+import{I as o}from"./index-BtcotG_Z.js";const t=o;export{t as component};
---
+++ assets/publications-CaWfCiLY.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BtcotG_Z.js";const e=function(){return o.jsx("div",{children:'Hello "/publications"!'})};export{e as component};
